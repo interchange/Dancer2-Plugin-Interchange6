9f0b6c85eab998a1ba163e7988eeda36bda4926a
Peter Mottram <peter@sysnix.com>
2016-03-21 19:11:24 +0100

    initial rehack of Routes::Checkout for plugin2

--- lib/Dancer2/Plugin/Interchange6/Routes/Checkout.pm
+++ lib/Dancer2/Plugin/Interchange6/Routes/Checkout.pm
@@ -1,15 +1,11 @@
 package Dancer2::Plugin::Interchange6::Routes::Checkout;
 
-use Dancer2::Plugin;
-use Dancer2::Plugin::Interchange6;
+use warnings;
+use strict;
 
 =head1 NAME
 
 Dancer2::Plugin::Interchange6::Routes::Checkout - Checkout routes for Interchange6 Shop Machine
-
-=cut
-
-register_hook 'before_checkout_display';
 
 =head1 DESCRIPTION
 
@@ -24,21 +20,25 @@ Returns the checkout route based on the passed routes configuration.
 =cut
 
 sub checkout_route {
+    my $app = shift;
     my $routes_config = shift;
+    my $d2pic6 = $app->with_plugin('Dancer2::Plugin::Interchange6');
 
     return sub {
         my %values;
 
         # add stuff useful for cart display
-        my $cart = cart;
+        my $cart = $d2pic6->cart;
         $values{cart_subtotal} = $cart->subtotal;
         $values{cart_total} = $cart->total;
         $values{cart} = $cart->products;
 
         # call before_checkout_display route so template tokens
         # can be injected
-        execute_hook('before_checkout_display', \%values);
-        template $routes_config->{checkout}->{template}, \%values;
+        $app->execute_hook( 'plugin.interchange6.before_checkout_display',
+            \%values );
+
+        $app->template( $routes_config->{checkout}->{template}, \%values );
     }
 }
 
